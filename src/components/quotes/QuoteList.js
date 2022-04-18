4dc88332eaf4f117266901b61b63f49f69cdd785
m-shvchk <thirdinthequeue@gmail.com>
2022-04-18 22:27:33 +0300

    added query parameters for sorting in QuoteList

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,41 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
-const QuoteList = ({quotes}) => {
+const QuoteList = ({ quotes }) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  // queryParams will return an object with key 'sort' and value 'asc' because of '/quotes?sort=asc'
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(quotes, isSortingAscending)
+
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
